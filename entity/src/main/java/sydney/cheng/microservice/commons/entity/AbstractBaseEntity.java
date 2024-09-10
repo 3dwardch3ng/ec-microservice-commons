0e1364fa148d54943668f3d99617dc5c1b09f6c9
Edward Cheng <edward@cheng.sydney>
2024-09-10 17:41:47 +1000

    add AbstractBaseEntity for common entity properties

---
+++ entity/src/main/java/sydney/cheng/microservice/commons/entity/AbstractBaseEntity.java
@@ -0,0 +1,32 @@
+package sydney.cheng.microservice.commons.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AbstractBaseEntity implements Serializable {
+    @Id
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
+    private String id;
+
+    @CreationTimestamp
+    private LocalDateTime creationTimestamp;
+
+    @UpdateTimestamp
+    private LocalDateTime updateTimestamp;
+}
